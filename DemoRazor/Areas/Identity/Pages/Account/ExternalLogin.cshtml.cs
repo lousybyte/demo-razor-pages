3162ff5a4ad8211f5797ec9cb99c5e0d5a62216c
lousybyte <lousybyte@users.noreply.github.com>
2022-01-27 10:44:35 +0200

    feat: add email confirmation for external login providers

--- DemoRazor/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ DemoRazor/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -3,6 +3,8 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using DemoRazor.Services;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -17,20 +19,20 @@ namespace DemoRazor.Areas.Identity.Pages.Account
     public class ExternalLoginModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser>   _userManager;
         private readonly ILogger<ExternalLoginModel> _logger;
+        private readonly EmailQueueService           _emailQueueSvc;
 
         public ExternalLoginModel(
             SignInManager<IdentityUser> signInManager,
             UserManager<IdentityUser> userManager,
             ILogger<ExternalLoginModel> logger,
-            IEmailSender emailSender)
+            EmailQueueService emailQueueSvc)
         {
             _signInManager = signInManager;
-            _userManager = userManager;
-            _logger = logger;
-            _emailSender = emailSender;
+            _userManager   = userManager;
+            _logger        = logger;
+            _emailQueueSvc = emailQueueSvc;
         }
 
         [BindProperty]
@@ -137,8 +139,7 @@ namespace DemoRazor.Areas.Identity.Pages.Account
                             values: new { area = "Identity", userId = userId, code = code },
                             protocol: Request.Scheme);
 
-                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        await _emailQueueSvc.QueueActivationEmailAsync(user, callbackUrl);
 
                         // If account confirmation is required, we need to show the link if we don't have a real email sender
                         if (_userManager.Options.SignIn.RequireConfirmedAccount)
